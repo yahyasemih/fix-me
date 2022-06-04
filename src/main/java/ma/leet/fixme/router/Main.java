cfe803b089e71eb3bf5bdc6a2f68e1c5fe1240e2
Yahya Ez-zainabi <yez-zain@student.1337.ma>
2022-06-04 22:19:44 +0100

    logging changes

---
+++ src/main/java/ma/leet/fixme/router/Main.java
@@ -0,0 +1,18 @@
+package ma.leet.fixme.router;
+
+import sun.misc.Signal;
+
+public class Main {
+  public static void main(String[] args) {
+    int brokersPort = 5000;
+    int marketsPort = 5001;
+    try (Router router = new Router(brokersPort, marketsPort)) {
+      sun.misc.Signal.handle(new Signal("INT"), sig -> router.setRunning(false));
+      sun.misc.Signal.handle(new Signal("TERM"), sig -> router.setRunning(false));
+      sun.misc.Signal.handle(new Signal("HUP"), sig -> router.setRunning(false));
+      router.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
--- src/main/java/ma/leet/fixme/router/Router.java
+++ src/main/java/ma/leet/fixme/router/Router.java
@@ -1,6 +1,7 @@
 package ma.leet.fixme.router;
 
 import java.io.Closeable;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -9,79 +10,125 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class Router implements Closeable {
-    private final ServerSocketChannel brokersChannel;
-    private final ServerSocketChannel marketsChannel;
-    private final Selector selector;
-    private static int ID = 99999;
-    private final AtomicBoolean isRunning;
+  private static final AtomicInteger ID = new AtomicInteger(99999);
+  private static final LogManager logManager = LogManager.getLogManager();
+  private final static Logger logger = Logger.getLogger("router");
 
-    public Router(int brokersPort, int marketsPort) throws Exception {
-        if (brokersPort == marketsPort) {
-            throw new InvalidParameterException("Markets and Brokers ports should be different");
+  static {
+    try {
+      logManager.readConfiguration(new FileInputStream("../logger.properties"));
+    } catch (IOException exception) {
+      logger.log(Level.SEVERE, "Cannot read configuration file", exception);
+    }
+  }
+
+  private final ServerSocketChannel brokersChannel;
+  private final ServerSocketChannel marketsChannel;
+  private final Selector selector;
+  private final AtomicBoolean isRunning;
+  private final List<SocketChannel> brokers;
+  private final List<SocketChannel> markets;
+  private final int brokersPort;
+  private final int marketsPort;
+
+  public Router(int brokersPort, int marketsPort) throws Exception {
+    if (brokersPort == marketsPort) {
+      throw new InvalidParameterException("Markets and Brokers ports should be different");
+    }
+    this.brokersPort = brokersPort;
+    this.marketsPort = marketsPort;
+    selector = Selector.open();
+    brokersChannel = ServerSocketChannel.open();
+    brokersChannel.configureBlocking(false);
+    brokersChannel.bind(new InetSocketAddress(brokersPort));
+    brokersChannel.register(selector, SelectionKey.OP_ACCEPT);
+    marketsChannel = ServerSocketChannel.open();
+    marketsChannel.configureBlocking(false);
+    marketsChannel.bind(new InetSocketAddress(marketsPort));
+    marketsChannel.register(selector, SelectionKey.OP_ACCEPT);
+    isRunning = new AtomicBoolean(true);
+    brokers = new ArrayList<>();
+    markets = new ArrayList<>();
+  }
+
+  public void start() throws Exception {
+    while (isRunning.get()) {
+      int selected = selector.select(100);
+      if (selected == 0) {
+        continue;
+      }
+      Iterator<SelectionKey> keys = selector.selectedKeys().iterator();
+      while (keys.hasNext()) {
+        SelectionKey key = keys.next();
+        keys.remove();
+        if (!key.isValid()) {
+          logger.info("closed connection");
+        } else if (key.isAcceptable()) {
+          acceptConnection(key);
+        } else if (key.isWritable()) {
+          logger.info("Writing...");
+        } else if (key.isReadable()) {
+          receiveMessage(key);
         }
-        selector = Selector.open();
-        brokersChannel = ServerSocketChannel.open();
-        brokersChannel.configureBlocking(false);
-        brokersChannel.bind(new InetSocketAddress(brokersPort));
-        brokersChannel.register(selector, SelectionKey.OP_ACCEPT);
-        marketsChannel = ServerSocketChannel.open();
-        marketsChannel.configureBlocking(false);
-        marketsChannel.bind(new InetSocketAddress(marketsPort));
-        marketsChannel.register(selector, SelectionKey.OP_ACCEPT);
-        isRunning = new AtomicBoolean(true);
+      }
     }
+  }
 
-    public void start() throws Exception {
-        while (isRunning.get()) {
-            int selected = selector.select(100);
-            if (selected == 0) {
-                continue;
-            }
-            Iterator<SelectionKey> keys = selector.selectedKeys().iterator();
-            while (keys.hasNext()) {
-                SelectionKey key = keys.next();
-                keys.remove();
-                if (!key.isValid()) {
-                    System.out.println("closed connection");
-                } else if (key.isAcceptable()) {
-                    System.out.println("Accepting connection");
-                    SocketChannel s = ((ServerSocketChannel) key.channel()).accept();
-                    //System.out.println(((InetSocketAddress)((ServerSocketChannel) key.channel()).getLocalAddress()).getPort());
-                    s.configureBlocking(false);
-                    s.register(selector, SelectionKey.OP_READ);
-                    ++ID;
-                    s.write(ByteBuffer.wrap(("Your id is " + ID + "\n").getBytes()));
-                } else if (key.isWritable()) {
-                    System.out.println("Writing...");
-                } else if (key.isReadable()) {
-                    System.out.println("Reading connection");
-                    ByteBuffer byteBuffer = ByteBuffer.allocate(5);
-                    SocketChannel s = (SocketChannel) key.channel();
-                    int read = s.read(byteBuffer);
-                    System.out.println("Read : " + read);
-                    if (read == -1) {
-                        s.close();
-                        key.cancel();
-                    } else {
-                        System.out.println(new String(byteBuffer.array()));
-                    }
-                }
-            }
-        }
+  private void receiveMessage(SelectionKey key) throws Exception {
+    logger.info("Reading connection");
+    ByteBuffer byteBuffer = ByteBuffer.allocate(5);
+    SocketChannel s = (SocketChannel) key.channel();
+    int read = s.read(byteBuffer);
+    logger.info("Read : " + read);
+    if (read == -1) {
+      int port = ((InetSocketAddress) (s.getLocalAddress())).getPort();
+      if (port == brokersPort) {
+        brokers.remove(s);
+      } else {
+        markets.remove(s);
+      }
+      s.close();
+      key.cancel();
+    } else {
+      logger.info(new String(byteBuffer.array()));
     }
+  }
 
-    public void setRunning(boolean running) {
-        isRunning.set(running);
+  private void acceptConnection(SelectionKey key) throws Exception {
+    SocketChannel s = ((ServerSocketChannel) key.channel()).accept();
+    logger.log(Level.INFO, "Accepting new connection from {0}",
+        ((InetSocketAddress) (s.getRemoteAddress())).getHostName());
+    if (((InetSocketAddress) (s.getLocalAddress())).getPort() == marketsPort) {
+      markets.add(s);
+    } else {
+      brokers.add(s);
     }
+    logger.info("markets : " + markets.size());
+    logger.info("brokers : " + brokers.size());
+    //System.out.println(((InetSocketAddress)((ServerSocketChannel) key.channel()).getLocalAddress()).getPort());
+    s.configureBlocking(false);
+    s.write(ByteBuffer.wrap(("Your id is " + ID.incrementAndGet() + "\n").getBytes()));
+    s.register(selector, SelectionKey.OP_READ);
+  }
 
-    @Override
-    public void close() throws IOException {
-        System.out.println("\rClosing and cleaning server...");
-        brokersChannel.close();
-        marketsChannel.close();
-    }
+  public void setRunning(boolean running) {
+    isRunning.set(running);
+  }
+
+  @Override
+  public void close() throws IOException {
+    logger.info("Closing and cleaning server...");
+    brokersChannel.close();
+    marketsChannel.close();
+  }
 }
