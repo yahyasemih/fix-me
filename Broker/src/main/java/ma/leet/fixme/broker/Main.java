79048255f6a0aa46191b34228f9876cc90a144b7
Yahya Ez-zainabi <yez-zain@student.1337.ma>
2022-08-26 22:56:15 +0100

    Improve broker usage

--- Broker/src/main/java/ma/leet/fixme/broker/Main.java
+++ Broker/src/main/java/ma/leet/fixme/broker/Main.java
@@ -1,14 +1,28 @@
 package ma.leet.fixme.broker;
 
+import static java.lang.Integer.parseInt;
+
 public class Main {
-  private static final String BROKER_HOST = "localhost";
-  private static final int BROKER_PORT = 5000;
   public static void main(String[] args) {
-    try (Broker broker = new Broker(BROKER_HOST, BROKER_PORT)) {
+    if (args.length < 1) {
+      System.out.println("Usage: broker router_host [router_port]");
+      return;
+    }
+    String routerHost = args[0];
+    int routerPort = 5000;
+    if (args.length >= 2) {
+      try {
+        routerPort = parseInt(args[1]);
+      } catch (NumberFormatException e) {
+        System.out.println("Invalid port : " + args[1]);
+        return;
+      }
+    }
+    try (Broker broker = new Broker(routerHost, routerPort)) {
       broker.buy("maticha", 5, "100000", 10);
       Thread.sleep(5000);
     } catch (Exception e) {
-      System.out.println("Error while trading : " + e.getMessage());
+      System.out.println("Error while trading : " + e);
     }
   }
 }
